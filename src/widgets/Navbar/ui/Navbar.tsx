b7ebe667116df14b5ebc8f1f301708ccce59052c
Ivan77web <ivanshestopalov39@gmail.com>
2023-02-09 19:49:44 +0300

    add babel, extract plugin

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useTranslation } from "react-i18next";
 import { classNames } from "shared/lib/classNames/classNames";
 import { AppLink, AppLinkTheme } from "shared/ui/AppLink/AppLink";
 import cl from "./Navbar.module.scss";
@@ -8,6 +9,8 @@ interface NavbarProps {
 }
 
 export const Navbar: React.FC<NavbarProps> = ({ className }) => {
+    const { t } = useTranslation("navbar");
+
     return (
         <div className={classNames(cl.navbar, {}, [className])}>
             <div className={cl.links}>
@@ -16,14 +19,18 @@ export const Navbar: React.FC<NavbarProps> = ({ className }) => {
                     to={"/"}
                     className={cl.mainLink}
                 >
-                    Главная страница
+                    {
+                        t("Главная страница")
+                    }
                 </AppLink>
 
                 <AppLink
                     theme={AppLinkTheme.SECONDARY}
                     to={"/about"}
                 >
-                    О сайте
+                    {
+                        t("О сайте")
+                    }
                 </AppLink>
             </div>
         </div>
