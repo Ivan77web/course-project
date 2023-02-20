6d4570f9777fc5412033b13ed47ac46452f9e503
Ivan77web <ivanshestopalov39@gmail.com>
2023-02-20 21:41:32 +0300

    TEST 6

--- .eslintrc.js
+++ .eslintrc.js
@@ -49,6 +49,7 @@ module.exports = {
         'jsx-a11y/alt-text': 'off',
         'jsx-a11y/click-events-have-key-events': 'off',
         'jsx-a11y/no-noninteractive-element-interactions': 'off',
+        'linebreak-style': 0,
     },
     globals: {
         __IS_DEV__: true,
