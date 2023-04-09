1a0b634ade4fbd9716455f718657b6f7b0232489
Ivan77web <ivanshestopalov39@gmail.com>
2023-04-09 13:48:38 +0300

    prod

--- src/pages/ArticlesDetailsPage/ui/ArticlesDetailsPage/ArticlesDetailsPage.async.tsx
+++ src/pages/ArticlesDetailsPage/ui/ArticlesDetailsPage/ArticlesDetailsPage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-// export const MainPageAsync = lazy(() => import('./MainPage'));
-export const ArticlesDetailsPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    setTimeout(() => resolve(import('./ArticleDetailsPage')), 1500);
-}));
+export const ArticlesDetailsPageAsync = lazy(() => import('./ArticlesDetailsPage'));
--- src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
+++ src/pages/ArticlesPage/ui/ArticlesPage/ArticlesPage.async.tsx
@@ -1,7 +1,3 @@
 import { lazy } from 'react';
 
-// export const MainPageAsync = lazy(() => import('./MainPage'));
-export const ArticlesDetailsPageAsync = lazy(() => new Promise((resolve) => {
-    // @ts-ignore
-    setTimeout(() => resolve(import('./ArticlesPage')), 1500);
-}));
+export const ArticlesDetailsPageAsync = lazy(() => import('./ArticlesPage'));
