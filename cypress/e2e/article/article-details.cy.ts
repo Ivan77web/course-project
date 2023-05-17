035f0b26883b88e7a7dcccb7954bb9362e73fb2b
Ivan77web <ivanshestopalov39@gmail.com>
2023-05-17 15:07:19 +0300

    add stubs

--- cypress/e2e/article/article-details.cy.ts
+++ cypress/e2e/article/article-details.cy.ts
@@ -13,7 +13,7 @@ describe('Пользователь заходит на страницу стат
         cy.getByTestId('ArticleDetails.Info').should('exist');
     });
 
-    it('И видит сипсок рекоммендаций', () => {
+    it('И видит список рекоммендаций', () => {
         cy.getByTestId('ArticleRecommendationsList').should('exist');
     });
 
@@ -25,6 +25,7 @@ describe('Пользователь заходит на страницу стат
     });
 
     it('И ставит оценку', () => {
+        cy.intercept('GET', '**/articles/*', { fixture: 'article-details.json' });
         cy.getByTestId('ArticleDetails.Info');
         cy.getByTestId('RatingCard').scrollIntoView();
         cy.setRate(5, 'feedback');
