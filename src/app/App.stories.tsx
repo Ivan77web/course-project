dff708aee20978505f1551a5af16699847a177b2
Ivan77web <ivanshestopalov39@gmail.com>
2023-05-30 13:50:17 +0300

    add all stories

--- src/app/App.stories.tsx
+++ src/app/App.stories.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
-import { Theme } from '@/shared/const/theme';
-import { ThemeDecorator } from '@/shared/config/storybook/ThemeDecorator/ThemeDecorator';
 import { StoreDecorator } from '@/shared/config/storybook/StoreDecorator/StoreDecorator';
 import App from './App';
+import { NewDesignDecorator } from '@/shared/config/storybook/NewDesignDecorator/NewDesignDecorator';
+import { UserRole } from '@/entities/User';
 
 export default {
     title: 'App',
@@ -21,8 +21,46 @@ Light.decorators = [StoreDecorator({
     loginForm: { username: '123', password: 'asd' },
 })];
 
-export const Dark = Template.bind({});
-Dark.args = {};
-Dark.decorators = [ThemeDecorator(Theme.DARK), StoreDecorator({
+export const LightAuth = Template.bind({});
+LightAuth.args = {};
+LightAuth.decorators = [StoreDecorator({
+    user: {
+        authData: {
+            id: '1',
+            username: 'admin',
+            roles: [
+                UserRole.ADMIN,
+            ],
+            avatar: 'https://sun9-36.userapi.com/impg/c855220/v855220920/217645/M_4Hrk9SIZ4.jpg?size=723x1080&quality=96&sign=907eeb1184d759743dc91b92133cc94a&type=album',
+        },
+    },
     loginForm: { username: '123', password: 'asd' },
 })];
+
+export const LightRedesigned = Template.bind({});
+LightRedesigned.args = {};
+LightRedesigned.decorators = [
+    StoreDecorator({
+        loginForm: { username: '123', password: 'asd' },
+    }),
+    NewDesignDecorator,
+];
+
+export const LightRedesignedAuth = Template.bind({});
+LightRedesignedAuth.args = {};
+LightRedesignedAuth.decorators = [
+    StoreDecorator({
+        user: {
+            authData: {
+                id: '1',
+                username: 'admin',
+                roles: [
+                    UserRole.ADMIN,
+                ],
+                avatar: 'https://sun9-36.userapi.com/impg/c855220/v855220920/217645/M_4Hrk9SIZ4.jpg?size=723x1080&quality=96&sign=907eeb1184d759743dc91b92133cc94a&type=album',
+            },
+        },
+        loginForm: { username: '123', password: 'asd' },
+    }),
+    NewDesignDecorator,
+];
